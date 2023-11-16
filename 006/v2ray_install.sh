4eb08db3196ff56fad0f6441407c805221d50d3a
aweill <84896083+aweill@users.noreply.github.com>
2023-11-16 11:02:25 +0800

    Update v2ray_install.sh

--- 006/v2ray_install.sh
+++ 006/v2ray_install.sh
@@ -68,9 +68,159 @@ identify_the_operating_system_and_architecture() {
 # 这个函数识别操作系统和架构，并根据检测到的信息设置相关的变量，如包管理器命令。
 
 
+# 6. judgment_parameters函数:
+## Demo function for processing parameters
+judgment_parameters() {
+  while [[ "$#" -gt '0' ]]; do
+    case "$1" in
+      '--remove')
+        if [[ "$#" -gt '1' ]]; then
+          echo 'error: Please enter the correct parameters.'
+          exit 1
+        fi
+        REMOVE='1'
+        ;;
+      '--version')
+        VERSION="${2:?error: Please specify the correct version.}"
+        break
+        ;;
+      '-c' | '--check')
+        CHECK='1'
+        break
+        ;;
+      '-f' | '--force')
+        FORCE='1'
+        break
+        ;;
+      '-h' | '--help')
+        HELP='1'
+        break
+        ;;
+      '-l' | '--local')
+        LOCAL_INSTALL='1'
+        LOCAL_FILE="${2:?error: Please specify the correct local file.}"
+        break
+        ;;
+      '-p' | '--proxy')
+        if [[ -z "${2:?error: Please specify the proxy server address.}" ]]; then
+          exit 1
+        fi
+        PROXY="$2"
+        shift
+        ;;
+      *)
+        echo "$0: unknown option -- -"
+        exit 1
+        ;;
+    esac
+    shift
+  done
+}
+
+# 这个函数使用while循环处理传递给脚本的所有参数。通过case语句检查每个参数的类型，然后根据不同的参数类型执行相应的操作。
+# 例如，--remove会设置REMOVE标志，--version会设置VERSION变量为指定的版本，-c会设置CHECK标志，以此类推。shift语句用于移除已经处理的参数。
+
+# 7. install_software函数:
+
+install_software() {
+  package_name="$1"
+  file_to_detect="$2"
+  type -P "$file_to_detect" > /dev/null 2>&1 && return
+  if ${PACKAGE_MANAGEMENT_INSTALL} "$package_name"; then
+    echo "info: $package_name is installed."
+  else
+    echo "error: Installation of $package_name failed, please check your network."
+    exit 1
+  fi
+}
+
+# 这个函数用于安装指定的软件包。它接收两个参数：软件包名称（package_name）和用于检测是否已安装的文件路径（file_to_detect）。
+# 首先，它使用type -P检测指定文件是否存在，如果存在则说明软件已安装，直接返回。如果文件不存在，就使用${PACKAGE_MANAGEMENT_INSTALL}执行包管理器安装软件包，如果安装失败则退出脚本。
 
 
+# 8. get_current_version函数:
 
+get_current_version() {
+  if /usr/local/bin/v2ray -version > /dev/null 2>&1; then
+    VERSION="$(/usr/local/bin/v2ray -version | awk 'NR==1 {print $2}')"
+  else
+    VERSION="$(/usr/local/bin/v2ray version | awk 'NR==1 {print $2}')"
+  fi
+  CURRENT_VERSION="v${VERSION#v}"
+}
+
+# 这个函数用于获取当前安装的v2ray版本。它首先尝试使用/usr/local/bin/v2ray -version获取版本号，如果失败则尝试使用/usr/local/bin/v2ray version。然后，通过awk命令提取版本号，将其存储在CURRENT_VERSION变量中。
+
+
+# 9. get_version函数:
+
+get_version() {
+  # 0: Install or update V2Ray.
+  # 1: Installed or no new version of V2Ray.
+  # 2: Install the specified version of V2Ray.
+  if [[ -n "$VERSION" ]]; then
+    RELEASE_VERSION="v${VERSION#v}"
+    return 2
+  fi
+  # Determine the version number for V2Ray installed from a local file
+  if [[ -f '/usr/local/bin/v2ray' ]]; then
+    get_current_version
+    if [[ "$LOCAL_INSTALL" -eq '1' ]]; then
+      RELEASE_VERSION="$CURRENT_VERSION"
+      return
+    fi
+  fi
+  # Get V2Ray release version number
+  TMP_FILE="$(mktemp)"
+  if ! curl -x "${PROXY}" -sS -i -H "Accept: application/vnd.github.v3+json" -o "$TMP_FILE" 'https://api.github.com/repos/v2fly/v2ray-core/releases/latest'; then
+    "rm" "$TMP_FILE"
+    echo 'error: Failed to get release list, please check your network.'
+    exit 1
+  fi
+  HTTP_STATUS_CODE=$(awk 'NR==1 {print $2}' "$TMP_FILE")
+  if [[ $HTTP_STATUS_CODE -lt 200 ]] || [[ $HTTP_STATUS_CODE -gt 299 ]]; then
+    "rm" "$TMP_FILE"
+    echo "error: Failed to get release list, GitHub API response code: $HTTP_STATUS_CODE"
+    exit 1
+  fi
+  RELEASE_LATEST="$(sed 'y/,/\n/' "$TMP_FILE" | grep 'tag_name' | awk -F '"' '{print $4}')"
+  "rm" "$TMP_FILE"
+  RELEASE_VERSION="v${RELEASE_LATEST#v}"
+  # Compare V2Ray version numbers
+  if [[ "$RELEASE_VERSION" != "$CURRENT_VERSION" ]]; then
+    RELEASE_VERSIONSION_NUMBER="${RELEASE_VERSION#v}"
+    RELEASE_MAJOR_VERSION_NUMBER="${RELEASE_VERSIONSION_NUMBER%%.*}"
+    RELEASE_MINOR_VERSION_NUMBER="$(echo "$RELEASE_VERSIONSION_NUMBER" | awk -F '.' '{print $2}')"
+    RELEASE_MINIMUM_VERSION_NUMBER="${RELEASE_VERSIONSION_NUMBER##*.}"
+    # shellcheck disable=SC2001
+    CURRENT_VERSION_NUMBER="$(echo "${CURRENT_VERSION#v}" | sed 's/-.*//')"
+    CURRENT_MAJOR_VERSION_NUMBER="${CURRENT_VERSION_NUMBER%%.*}"
+    CURRENT_MINOR_VERSION_NUMBER="$(echo "$CURRENT_VERSION_NUMBER" | awk -F '.' '{print $2}')"
+    CURRENT_MINIMUM_VERSION_NUMBER="${CURRENT_VERSION_NUMBER##*.}"
+    if [[ "$RELEASE_MAJOR_VERSION_NUMBER" -gt "$CURRENT_MAJOR_VERSION_NUMBER" ]]; then
+      return 0
+    elif [[ "$RELEASE_MAJOR_VERSION_NUMBER" -eq "$CURRENT_MAJOR_VERSION_NUMBER" ]]; then
+      if [[ "$RELEASE_MINOR_VERSION_NUMBER" -gt "$CURRENT_MINOR_VERSION_NUMBER" ]]; then
+        return 0
+      elif [[ "$RELEASE_MINOR_VERSION_NUMBER" -eq "$CURRENT_MINOR_VERSION_NUMBER" ]]; then
+        if [[ "$RELEASE_MINIMUM_VERSION_NUMBER" -gt "$CURRENT_MINIMUM_VERSION_NUMBER" ]]; then
+          return 0
+        else
+          return 1
+        fi
+      else
+        return 1
+      fi
+    else
+      return 1
+    fi
+  elif [[ "$RELEASE_VERSION" == "$CURRENT_VERSION" ]]; then
+    return 1
+  fi
+}
+
+# 这个函数用于确定是否需要安装新的V2Ray版本。首先，如果用户通过--version指定了版本，那么RELEASE_VERSION会被设置为指定的版本，并且函数返回值为2。接着，如果本地已安装V2Ray，通过get_current_version获取当前版本。
+# 如果是本地安装，则RELEASE_VERSION被设置为当前版本。然后，通过GitHub API获取V2Ray最新的版本号，并进行版本比较。如果需要安装新版本，返回值为0。
 
 
 
